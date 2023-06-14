b3ccdc3a384b6184340ca8d594cacd66cf308d41
askvart <tur1amant@gmail.com>
2023-06-14 11:47:16 +0300

    refactoring1

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,11 +8,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ('username', 'email')
 
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ('username', 'email')
